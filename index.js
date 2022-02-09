fe91ef4339694f28161aca8920c1b2c06f838093
Emily <emilybribragg@gmail.com>
2022-02-09 14:37:09 -0500

    initialized submit event listener, added fetch function to DOMContentLoaded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //DOM Content Loaded Event Listener & Corresponding Functions
 document.addEventListener('DOMContentLoaded', () => {
-  
+  fetchBreweries();
 })
 
 // Fetch the data
@@ -13,4 +13,17 @@ function fetchBreweries() {
         breweries = data;
       })
       .catch(error => console.log(error))
+}
+
+// "Submit" Event Listener For the Selected State
+let resultsInState = []
+function submitListener() {
+  const breweryForm = document.getElementById('brewery-form')
+  breweryForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      resultsInState = []
+      const state = document.getElementById('states')
+      const selectedState = [state].map(option => option.value)
+      renderStates(selectedState[0]) 
+  })
 }
