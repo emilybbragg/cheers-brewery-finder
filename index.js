28a203877ea1690740de20fd1deec9eb4fbfd33a
Emily <emilybribragg@gmail.com>
2022-02-09 14:44:15 -0500

    function to render states, began code for like button

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ function submitListener() {
       renderStates(selectedState[0]) 
   })
 }
+
+//Function to Render States
+const list = document.getElementById('brewery-list')
+function renderStates(selectedState) {
+  const EMPTY_HEART = '♡'
+  const FULL_HEART = '♥'
+
+  const likeBrewery = (e) => {
+    const liker = e.target
+    const like = liker.textContent
+    if (like === EMPTY_HEART) {
+      liker.textContent = FULL_HEART
+    } else {
+      liker.textContent = EMPTY_HEART
+    }
+  }
